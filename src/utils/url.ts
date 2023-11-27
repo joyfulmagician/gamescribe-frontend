2b883f48b8ee0f42c480c714ac32e43c0db2e4bc
vegasnc <vegaslancer1025@gmail.com>
2023-11-27 11:42:58 -0800

    [Merge] replace master branch entirely from feature/nextjs branch

---
+++ src/utils/url.ts
@@ -0,0 +1,14 @@
+const baseUrl = "https://gamescribe.kneeshaw-developments.com";
+// const baseUrl = "http://localhost:5000";
+
+const URLCONST = {
+  registerAPI: baseUrl + "/signup",
+  loginAPI: baseUrl + "/login",
+  generateContentAPI: baseUrl + "/generate_content",
+  generateQuestionAPI: baseUrl + "/generate_question",
+  saveUpdatedContentAPI: baseUrl + "/save_updated_content",
+  getHexagonDataAPI: baseUrl + "/hexagon_data",
+  getMonstersAPI: baseUrl + "/get/monsters",
+};
+
+export { URLCONST };
